399b7f305acaab9bd4e6c5db6ff126139be0d984
Gabriel Scherer <gabriel.scherer@gmail.com>
2016-05-13 16:31:40 -0400

    initial import: Haskell program

---
+++ Main.hs
@@ -0,0 +1,27 @@
+module Main (main) where
+
+import qualified Control.Exception as Exception
+import qualified Control.Monad as Monad
+import qualified Data.ByteString as ByteString
+import qualified Data.Map.Strict as Map
+
+data Msg = Msg !Int !ByteString.ByteString
+
+type Chan = Map.Map Int ByteString.ByteString
+
+message :: Int -> Msg
+message n = Msg n (ByteString.replicate 1024 (fromIntegral n))
+
+pushMsg :: Chan -> Msg -> IO Chan
+pushMsg chan (Msg msgId msgContent) =
+  Exception.evaluate $
+    let
+      inserted = Map.insert msgId msgContent chan
+    in
+      if 200000 < Map.size inserted
+      then Map.deleteMin inserted
+      else inserted
+
+main :: IO ()
+main = Monad.foldM_ pushMsg Map.empty (map message [1..1000000])
+
