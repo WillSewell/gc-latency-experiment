29cb63b66c989a9f8e28953bae4529efa53a8fab
Gabriel Scherer <gabriel.scherer@gmail.com>
2016-05-13 16:32:35 -0400

    have the OCaml program keep its own time

--- main.ml
+++ main.ml
@@ -9,14 +9,21 @@ let message n = { id = n; content = String.make 1024 (Char.chr (n mod 256)) }
 let worst = ref 0.
 
 let push_msg (count, map) msg =
+  let before = Unix.gettimeofday () in
   let inserted = IMap.add msg.id msg.content map in
   let count = count + 1 in
-  if 200_000 < count
-  then (count - 1, snd (IMap.pop_min_binding inserted))
-  else (count, inserted)
-  
-let () =
-  Seq.init 1_000_000 message
-  |> Seq.fold_left push_msg (0, IMap.empty)
-  |> ignore
+  let result =
+    if 200_000 < count
+    then (count - 1, snd (IMap.pop_min_binding inserted))
+    else (count, inserted) in
+  let after = Unix.gettimeofday () in
+  worst := max !worst (after -. before);
+  result
 
+let () =
+  begin
+    Seq.init 1_000_000 message
+    |> Seq.fold_left push_msg (0, IMap.empty)
+    |> ignore
+  end;
+  Printf.printf "%.2E\n" !worst
