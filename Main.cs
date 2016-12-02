6a9cbe87a97c8334e5c812a42abc7558954b3101
Franck Jeannin <franck.jeannin@datanuage.com>
2016-12-02 12:06:14 +0100

    Added C# version, changed msgCount to 10M to get consistent results

---
+++ Main.cs
@@ -0,0 +1,46 @@
+﻿namespace gc_latency_experiment
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    class Main_
+    {
+        private const int windowSize = 200000;
+        private const int msgCount = 10000000;
+        private const int msgSize = 1024;
+
+        private static byte[] createMessage(int n)
+        {
+            return Enumerable.Repeat((byte)n, msgSize).ToArray();
+        }
+
+        private static void pushMessage(Dictionary<int, byte[]> map, int id)
+        {
+            var lowId = id - windowSize;
+            map.Add(id, createMessage(id));
+            if (lowId >= 0)
+            {
+                map.Remove(lowId);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var worst = new TimeSpan();
+            var map = new Dictionary<int, byte[]>();
+            for (var i = 0; i < msgCount; i++)
+            {
+                var sw = Stopwatch.StartNew();
+                pushMessage(map, i);
+                sw.Stop();
+                if (sw.Elapsed > worst)
+                {
+                    worst = sw.Elapsed;
+                }
+            }
+
+            Console.WriteLine($"Worst push time : {worst.TotalMilliseconds} ms");
+        }
+    }
+}
