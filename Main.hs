6af1e4a7ba0cea64ccd731feebb814b50e3da128
Will Sewell <me@willsewell.com>
2017-10-11 15:27:29 +0100

    Time Haskell manually

--- Main.hs
+++ Main.hs
@@ -1,8 +1,10 @@
 module Main (main) where
-
+  
 import qualified Control.Exception as Exception
+import qualified Control.Monad as Monad
 import qualified Data.Array.IO as Array
 import qualified Data.ByteString as ByteString
+import qualified Data.Time.Clock as Clock
 
 type Msg = ByteString.ByteString
 
@@ -14,12 +16,17 @@ msgCount = 1000000
 message :: Int -> Msg
 message n = ByteString.replicate 1024 (fromIntegral n)
 
-pushMsg :: Chan -> Int -> IO ()
-pushMsg chan highId = do
+pushMsg :: Chan -> Clock.NominalDiffTime -> Int -> IO Clock.NominalDiffTime
+pushMsg chan worst highId = do
+    start <- Clock.getCurrentTime
     m <- Exception.evaluate $ message highId
     Array.writeArray chan (highId `mod` windowSize) m
+    end <- Clock.getCurrentTime
+    let elapsed = Clock.diffUTCTime end start
+    return $ max elapsed worst
 
 main :: IO ()
 main = do
   c <- Array.newArray_ (0, windowSize)
-  mapM_ (pushMsg c) [0..msgCount]
+  worst <- Monad.foldM (pushMsg c) 0 [0..msgCount]
+  print worst
