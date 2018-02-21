68b2764b1f08b44a37706cdc75e888aceddefe58
gillchristian <gillchristiang@gmail.com>
2018-02-21 15:16:19 +0100

    Fix formating & update mkMessage

--- node/main.js
+++ node/main.js
@@ -1,36 +1,32 @@
-const	windowSize = 200000
-const msgCount   = 1000000
-let worst        = -Infinity
+const windowSize = 200000;
+const msgCount = 1000000;
+let worst = -Infinity;
 
 function mkMessage(n) {
-  const m = []
-  for (let i; i < 2014; i++) {
-    m.push(n)
-  }
-	return m
+  return new Uint8Array(1024).fill(n % 255);
 }
 
 function pushMsg(c, highID) {
-	const start = Date.now()
-	const m = mkMessage(highID)
+  const start = Date.now();
+  const m = mkMessage(highID);
 
-	c[highID%windowSize] = m
+  c[highID % windowSize] = m;
 
-	const elapsed = Date.now() - start
+  const elapsed = Date.now() - start;
 
-	if (elapsed > worst) {
-		worst = elapsed
-	}
+  if (elapsed > worst) {
+    worst = elapsed;
+  }
 }
 
 function main() {
-	const c = []
+  const c = [];
 
-	for (let i = 0; i < msgCount; i++) {
-		pushMsg(c, i)
-	}
+  for (let i = 0; i < msgCount; i++) {
+    pushMsg(c, i);
+  }
 
-	console.log(`Worst push time: ${worst}ms`)
+  console.log(`Worst push time: ${worst}ms`);
 }
 
-main()
+main();
