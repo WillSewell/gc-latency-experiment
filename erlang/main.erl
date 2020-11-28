64a8e7769931218a3fd85646ad762bad26dc2426
Filipe Varjão <varjaofilipe@gmail.com>
2020-11-28 17:42:26 -0300

    [fix] erlang benchmark

--- erlang/main.erl
+++ erlang/main.erl
@@ -8,10 +8,15 @@ push_message(1000000, _, Worst) -> io:format("Worst push time: ~p~n", [Worst]);
 push_message(MsgCount, Store, Worst) ->
     Start = time_microseg(),
     NewStore = create_message(MsgCount),
-    End = time_microseg(),
-    case (End - Start) > Worst of
-        true -> push_message(MsgCount + 1, [NewStore|Store], (End - Start));
-        false -> push_message(MsgCount + 1, Store, Worst)
+    ElapsedMs = time_microseg() - Start,
+    case {ElapsedMs > Worst, length(Store)} of
+        {true, 200000} -> 
+            [_|TStore] = Store,
+            push_message(MsgCount + 1, TStore ++ NewStore, ElapsedMs);
+        {true, _} ->
+            push_message(MsgCount + 1, [NewStore|Store], ElapsedMs);
+        {false, _} ->
+            push_message(MsgCount + 1, Store, Worst)
     end.
 
 create_message(I) -> create_message(1024, I, []).
