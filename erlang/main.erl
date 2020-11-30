cbe56fa9d5dbc48494a720998a7e5934b0db904d
Filipe Varjão <varjaofilipe@gmail.com>
2020-11-30 10:29:47 -0300

    [fix]
    Co-authored-by: Håkan Nilsson <hakan.nilsson@klarna.com>

--- erlang/main.erl
+++ erlang/main.erl
@@ -1,29 +1,21 @@
 -module(main).
 -export([start/0]).
 
+-define(window_size, 200000).
+-define(msg_count, 1000000).
+
+message(N) ->
+    list_to_binary(lists:duplicate(1024, N rem 256)).
+
+push_message(_Channel, Worst, ?msg_count) ->
+    io:format("Worst push time: ~p~n", [Worst]);
+push_message(Channel, Worst, Id) ->
+    {Elapsed, NewChannel} =
+        timer:tc(fun() ->
+                         array:set(Id rem ?window_size, message(Id), Channel)
+                 end),
+    push_message(NewChannel, max(Worst, Elapsed), Id + 1).
+
 start() ->
-    push_message(0, [], 0).
-
-push_message(1000000, _, Worst) -> io:format("Worst push time: ~p~n", [Worst]);
-push_message(MsgCount, Store, Worst) ->
-    Start = time_microseg(),
-    NewStore = create_message(MsgCount),
-    ElapsedMs = time_microseg() - Start,
-    case {ElapsedMs > Worst, length(Store)} of
-        {true, 200000} -> 
-            [_|TStore] = Store,
-            push_message(MsgCount + 1, TStore ++ NewStore, ElapsedMs);
-        {true, _} ->
-            push_message(MsgCount + 1, [NewStore|Store], ElapsedMs);
-        {false, _} ->
-            push_message(MsgCount + 1, Store, Worst)
-    end.
-
-create_message(I) -> create_message(1024, I, []).
-
-create_message(0, _, Bytes) -> Bytes;
-create_message(Size, I, Bytes) ->
-    create_message(Size - 1, I , [I|Bytes]).
-
-time_microseg() ->
-    erlang:system_time('micro_seconds').
+    Channel = array:new(?window_size, {default, 0}),
+    push_message(Channel, 0, 0).
